f55730aee82fd4c66bec945f3d9c444bd5bc9d8f
Ayush <ayushmishra7484@gmail.com>
2025-04-18 23:34:51 +0530

    basic chat model built

--- 3_Chat_model/conversations.py
+++ 3_Chat_model/conversations.py
@@ -1,12 +1,27 @@
 from langchain_google_genai import ChatGoogleGenerativeAI as genAI
 from dotenv import load_dotenv
 load_dotenv()
-from langchain_core.messages import HumanMessage, SystemMessage
-
-messages=[
-    SystemMessage(content="You are expert in social media marketing"),
-    HumanMessage(content="Give a short tip to increase engaging posts on instagram")
-]
+from langchain_core.messages import HumanMessage, SystemMessage,AIMessage
 model=genAI(model="gemini-1.5-pro")
-result=model.invoke(messages)
-print( result.content)
+
+chat_history = []
+
+SystemMessage=SystemMessage(content="You are an helpful AI assistant")
+chat_history.append(SystemMessage)
+
+#chat loop
+
+while True:
+    query=input("You: ")
+    if query.lower() == "exit":
+        break
+    chat_history.append(HumanMessage(content=query))
+
+    result = model.invoke(chat_history)
+    response=result.content
+    chat_history.append(AIMessage(content=response))
+    print("AI: ", response)
+
+
+    # print("Chat History:")
+    # print(chat_history)
